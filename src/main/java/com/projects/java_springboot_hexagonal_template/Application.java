330d86dc289022076aa004a54b473d0d08877189
Sandro Mendoza <sandro.mendoza@ffufm.com>
2026-03-20 14:41:22 +0800

    Initial commit

---
+++ src/main/java/com/projects/java_springboot_hexagonal_template/Application.java
@@ -0,0 +1,13 @@
+package com.projects.java_springboot_hexagonal_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
